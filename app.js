0d81db44ac71a29915407458d9a35eacda45b364
Huy Truong <huy.truong@mevry.com>
2020-07-04 08:01:02 +0700

    fetch fanpage data from url

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
+const express = require('express')
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.nhaccuatui.com/bai-hat/top-20.html");
+const app = express()
 
-  const songs = await page.evaluate(() => {
-    let items = document.querySelectorAll(".name_song");
-    let links = [];
-    items.forEach(item => {
-      links.push({
-        title: item.innerText,
-        url: item.getAttribute("href")
-      });
-    });
-    return links;
-  });
+const port = process.env.PORT || 3000
+
+
+app.get('/fanpage', (req, res) => {
+    const { url } = req.query;
+    let fanpageData = {};
+
+  (async () => {
+    // const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitFor(2000)
+    fanpageData = await page.evaluate(() => {
+      const backgroundImageElement = document.querySelector("._4on7");
+      // ._u9q : about section
+      let addressElement = document.querySelector('._u9q > div:nth-child(3) ._4bl9 > div')
+      let phoneNumberElement = document.querySelector('._u9q > div:nth-child(4) ._4bl9 > div')
+      let openTimeElement = document.querySelector('._u9q > div:nth-child(6) ._4bl9 > div')
+      let numOfLikesElement = document.querySelector('._6590 > div:nth-child(2) ._4bl9 > div')
+      let numOfFollowersElement = document.querySelector('._6590 > div:nth-child(3) ._4bl9 > div')
+      // let rateReviewElement = document.querySelector('._1c02')
+      // let reviewDataElement = [...document.querySelectorAll('._3-96 ._2w09 > div > span')]
   
-  for (let song of songs) {
-    await page.goto(song.url);
-    let lyric = await page.evaluate(() => {
-      let lyric = document
-        .getElementsByClassName("pd_lyric trans")[0]
-        .innerHTML.replace(/\<br\>/g, "");
-      return lyric;
+      // const reviewData = { rate: reviewDataElement[0],
+          // numOfReviewers: reviewDataElement[1].innerText.match(/\d*(?= người)/g)[0]}
+      const imageUrl = backgroundImageElement && backgroundImageElement.src
+      const address = addressElement && addressElement.innerText
+      const phoneNumber = phoneNumberElement && phoneNumberElement.innerText
+      const openTime = openTimeElement && openTimeElement.innerText
+      // const rateReview = rateReviewElement && rateReviewElement.innerText
+  
+      // 8.615 người thích trang này => 8615
+      const numOfLikes = numOfLikesElement && numOfLikesElement.innerText.replace('.', '').match(/\d*(?= người)/g)[0]
+      const numOfFollowers = numOfFollowersElement && numOfFollowersElement.innerText.replace('.', '').match(/\d*(?= người)/g)[0]
+  
+      return {imageUrl, address, phoneNumber, openTime, numOfLikes, numOfFollowers, numOfLikes};
     });
-    console.log(song.title);
-    console.log("..............................");
-    console.log(lyric);
-  }
+    
+    res.send({ fanpageData });
+    await browser.close();
+  })();
+  });
 
-  await browser.close();
-})();
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}!`)
+})
+
+// require('./fanpageFb')
